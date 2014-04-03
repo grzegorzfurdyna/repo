9b9be72ab4eb656cea39ea134ab1a77bb94f1a55
grzegorzfurdyna <grzegorzfurdyna1@gmail.com>
2014-04-03 17:06:49 +0200

    Menu
    Menu

---
+++ SprintOne/Assets/Menu.js
@@ -0,0 +1,26 @@
+﻿var isQuitButton = false;
+var enter : Sprite;
+var exit : Sprite;
+
+function OnMouseEnter()
+{
+GetComponent(SpriteRenderer).sprite = enter;
+}
+
+function OnMouseExit()
+{
+GetComponent(SpriteRenderer).sprite = exit;
+}
+
+function OnMouseUp()
+{
+if( isQuitButton )
+{
+Application.Quit();
+}
+else
+{
+Application.LoadLevel(1);
+}
+}
+Screen.showCursor = true;
