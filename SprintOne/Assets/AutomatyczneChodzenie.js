011bdd38071329423dcce4dfc1a716e4eef2fb9d
grzegorzfurdyna <grzegorzfurdyna1@gmail.com>
2014-04-24 13:49:10 +0200

    Zmiany
    Zmiany

--- SprintOne/Assets/AutomatyczneChodzenie.js
+++ SprintOne/Assets/AutomatyczneChodzenie.js
@@ -1,4 +1,8 @@
+
+
 var Speed : float = 10; /// Szybkość biegania
+GetComponent(Transform).position.x = 63;
+GetComponent(Transform).position.y = -7.213;
 function Update () { 
-rigidbody2D.velocity.x = Speed;
+rigidbody2D.velocity.x = (-1) * Speed;
 }
